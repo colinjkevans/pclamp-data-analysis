e87bf773d7a8af1dcbfc594a3fc668e019d262f9
mattisj <joanna.mattis@uphs.upenn.edu>
2019-04-10 17:36:59 -0400

    Write output to file

--- joannas_experiment.py
+++ joannas_experiment.py
@@ -3,14 +3,18 @@ from analyze_abf import CurrentStepsData
 
 
 ABF_LOCATION = r'C:\Users\mattisj\Desktop\9-Patching\GC patching Cl free internal\adult WT\JM20190327_0034.abf'
+OUTPUT_FILE = r'C:\Users\mattisj\Desktop\output_file.csv'
 
 abf = ABF(ABF_LOCATION)
 experiment = CurrentStepsData(abf)
 
-for sweep in experiment.sweeps:
-    ap_count = sweep.find_output_peaks()
-    base_level, hat_level, hat_mid, hat_width = sweep.fit_input_tophat(verify=True)
-    current_step = hat_level - base_level
-    print('Current: {}pA, APs: {}'.format(current_step, len(ap_count)))
+with open(OUTPUT_FILE, 'w') as f:
+    for sweep in experiment.sweeps:
+        aps = sweep.find_output_peaks()
+        base_level, hat_level, hat_mid, hat_width = sweep.fit_input_tophat()
+        current_step = hat_level - base_level
+        print('Current: {}pA, APs: {}'.format(current_step, len(aps)))
+        f.write('{}, {}\n'.format(current_step, len(aps)))
+
 
 
